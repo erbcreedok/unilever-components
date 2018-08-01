cfaa84f9cc5d3fbd3c9d7ec1b9a92d104e1b00f5
erbcreedok <erbcreedok@gmail.com>
2018-08-01 14:05:03 +0600

    Bekzhan and Yerbols Modules Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import {ProfileWindowComponent} from './profile/profile-window/profile-window.component';
+
+import { NgModule } from '@angular/core';
+import { ElModule } from 'element-angular/release/element-angular.module';
+import { BekzhanModule } from './bekzhan-components/bekzhan.module';
+import { YerbolModule } from './yerbol-components/yerbol.module';
+
+import { ProfileWindowComponent } from './profile/profile-window/profile-window.component';
+
+import 'element-angular/theme/index.css'
 
 @NgModule({
   declarations: [
@@ -10,7 +17,11 @@ import {ProfileWindowComponent} from './profile/profile-window/profile-window.co
     ProfileWindowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ElModule.forRoot(),
+    BekzhanModule, // Тестовый модуль Бекжана подгружается с bekzhan.module.ts
+    YerbolModule, // Тестовый модуль Ербола подгружается с yerbol.module.ts
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bekzhan-components/bekzhan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-bekzhan',
+    templateUrl: './bekzhan.component.html',
+    styleUrls: ['./bekzhan.component.scss']
+})
+export class BekzhanComponent {
+    title = 'Bekzhan local Component';
+}
