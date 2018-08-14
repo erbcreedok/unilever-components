31ca56e94434788d4199662d430d8bf6d3839c5a
bomba65 <bkalzhan@gmail.com>
2018-08-14 11:06:04 +0600

    Bekzhan(Tabs)

--- src/app/bekzhan/bekzhan.module.ts
+++ src/app/bekzhan/bekzhan.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BekzhanComponent } from './bekzhan.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TabComponent } from './tabs/tab.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BekzhanComponent],
+  declarations: [BekzhanComponent, TabsComponent, TabComponent],
   exports: [BekzhanComponent]
 })
 export class BekzhanModule { }
