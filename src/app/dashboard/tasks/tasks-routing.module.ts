bd1a7d3801350f69ba52204a1d63cba6ffc5952d
erbcreedok <erbcreedok@gmail.com>
2018-10-11 18:40:41 +0600

    task routes fix

--- src/app/dashboard/tasks/tasks-routing.module.ts
+++ src/app/dashboard/tasks/tasks-routing.module.ts
@@ -6,21 +6,20 @@ import {OtherTasksComponent} from './other-tasks/other-tasks.component';
 import {TasksComponent as TaskContainerComponent} from '../../containers/tasks/tasks.component';
 import {MyTasksTableComponent} from './my-tasks/my-tasks-table/my-tasks-table.component';
 import {TaskComponent} from './task/task.component';
-import {STATUSES, TYPES} from './task-types.constants';
 
 const routes: Routes = [
   {path: 'id/:type/:id', component: TaskComponent},
   { path: '', component: TasksComponent, children: [
     {path: 'my', component: MyTasksComponent, children: [
       {path: ':status/:type', component: MyTasksTableComponent},
-      {path: ':status', redirectTo: ':status/' + TYPES[0].path},
-      {path: '', redirectTo: STATUSES[0].path + '/' + TYPES[0].path},
+      {path: ':status', redirectTo: ':status/' + 'promoactivities'},
+      {path: '', redirectTo: 'in-process' + '/' + 'promoactivities'},
     ]},
     {path: 'other', component: OtherTasksComponent, children: [
       {path: ':type', component: TaskContainerComponent},
-      {path: '', redirectTo: TYPES[0].path},
+      {path: '', redirectTo: 'promoactivities'},
     ]},
-    {path: '', redirectTo: 'my/' + STATUSES[0].path},
+    {path: '', redirectTo: 'my/' + 'in-process'},
   ]},
 ];
 
